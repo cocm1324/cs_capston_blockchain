2010b77661c2dd0aa48354497251438755825a1f
Sapphire317 <sdw9090@naver.com>
2019-02-09 20:04:18 +0800

    only election

--- elss/lib/script_election.js
+++ elss/lib/script_election.js
@@ -38,14 +38,39 @@ function createElection(ElectionData){
     })
 }
 
-
-
 function generateElectionKey(school){
-    var d = new Date(2018,04,05,06,0,0,0,0);
+    var dt = new Date(2018,04,05,06,0,0,0,0);
 
-    var year = dt.getYear().substring(2,4);
+    var year = dt.getFullYear()%100;
     var month = dt.getMonth()+1;
     var dayNum = dt.getDate();
 
     return school+year+month+dayNum+'Election';
+}
+
+
+/**
+ * 
+ * Change the value of Attendance in StudentInfo Transaction
+ * @param {org.elss.election.changeElecStatus} statusData
+ * @transaction
+ */
+function changeElecStatus(statusData){
+    var statusRegistry={}
+    
+    return getAssetRegistry('org.elss.election.Election').then(function(registry){
+        statusRegistry = registry
+        return statusRegistry.get(statusData.key);
+    }).then(function(status){
+        if(!status) throw new Error("Status : "+statusData,key," Not Found!!!");
+        status.status= statusData.status;
+        return statusRegistry.update(status);
+    }).then(function(){
+        // Successful update
+        var event = getFactory().newEvent('org.elss.election', 'statusChanged');
+        event.status = statusData.status;
+        emit(event);
+    }).catch(function(error){
+        throw new Error(error);
+    });
 }
