5d8bb0dfe8762b69770e8b0f0c224e0c73e6bba6
Sapphire317 <sdw9090@naver.com>
2019-02-09 14:51:09 +0800

    createElection

--- elss/lib/script.js
+++ elss/lib/script.js
@@ -1,180 +1,155 @@
-/**
- * 
- * Create Election Transaction
- * @param {org.elss.election.createElection} ElectionData
- * @transaction
- */
+// /**
+//  * 
+//  * Create Election Transaction
+//  * @param {org.elss.election.createElection} ElectionData
+//  * @transaction
+//  */
 
+// function createElection(ElectionData){
+//     return getAssetRegistry('org.elss.election.Election')
+//     .then(function(electionRegistry){
 
-// asset Election identified by key{
-//     o String key
-//     o Schedule schedule
-//     o ElectionInfo info
-//     --> ElectionLedger electionledger optional
-//     --> VotingBox votingbox optional
-//     --> Attendance attendance optional
-//   }
-  
-// concept Schedule {
-//     o DateTime Date
-//     o School school
-//     o String name
-//   }
-// transaction createElection {
-//     o School school
-//     o String name
-//     o Double quorumRate
-//   }
-  
-//   event electionCreated{
-//     o String key
-//   }
+//        //Now add the Election
+//        var factory = getFactory();
+//        var ns = 'org.elss.election';
+//        var ns_common = 'org.elss.common';
 
-function createElection(ElectionData){
-    return getAssetRegistry('org.elss.election.Election')
-    .then(function(electionRegistry){
-
-       //Now add the Election
-       var factory = getFactory();
-       var ns = 'org.elss.election';
-       var ns_common = 'org.elss.common';
-
-       var electionKey = generateElectionKey(ElectionData.school);
-       var election = factory.newResource(ns, 'Election', electionKey);
+//        var electionKey = generateElectionKey(ElectionData.school);
+//        var election = factory.newResource(ns, 'Election', electionKey);
        
-       var schedule = factory.newConcept(ns_common,'Schedule');
-       schedule.name = ElectionData.name;
-       schedule.school = ElectionData.school;
-       schedule.expiry = ElectionData.expiry;
-       election.schedule = schedule;
+//        var schedule = factory.newConcept(ns_common,'Schedule');
+//        schedule.name = ElectionData.name;
+//        schedule.school = ElectionData.school;
+//        schedule.expiry = ElectionData.expiry;
+//        election.schedule = schedule;
           
-       var electionInfo = factory.newConcept(ns, 'ElectionInfo');
-       electionInfo.quorumRate = ElectionData.quorumRate;
-       election.info = electionInfo;
+//        var electionInfo = factory.newConcept(ns, 'ElectionInfo');
+//        electionInfo.quorumRate = ElectionData.quorumRate;
+//        election.info = electionInfo;
 
-       var event = factory.newEvent(ns, 'electionCreated');
-       event.key = electionKey;
-       emit(event);
+//        var event = factory.newEvent(ns, 'electionCreated');
+//        event.key = electionKey;
+//        emit(event);
 
-       return electionRegistry.add(election);
+//        return electionRegistry.add(election);
 
-    })
-}
-function generateElectionKey(school){
-    var dt = new Date.now();
+//     })
+// }
+// function generateElectionKey(school){
+//     var dt = new Date.now();
 
-    var month = dt.getMonth();
-    var dayNum = dt.getDate();
+//     var month = dt.getMonth();
+//     var dayNum = dt.getDate();
 
-    return school+'-'+month+'-'+dayNum;
-}
+//     return school+'-'+month+'-'+dayNum;
+// }
 
-/**
-* 
-* Create Election Transaction
-* @param {org.elss.election.assignAll} assignData
-* @transaction
-*/
+// /**
+// * 
+// * Create Election Transaction
+// * @param {org.elss.election.assignAll} assignData
+// * @transaction
+// */
 
-// asset Election identified by key{
-//     o String key
-//     o Schedule schedule
-//     o ElectionInfo info
-//     --> ElectionLedger electionledger optional
-//     --> VotingBox votingbox optional
-//     --> Attendance attendance optional
-//   }
+// // asset Election identified by key{
+// //     o String key
+// //     o Schedule schedule
+// //     o ElectionInfo info
+// //     --> ElectionLedger electionledger optional
+// //     --> VotingBox votingbox optional
+// //     --> Attendance attendance optional
+// //   }
 
-function AssignAll(assigndata){
-    var electionRegistry={}
-    return getAssetRegistry('org.elss.election.Election').then(function(registry){
-        electionRegistry = registry
-        return electionRegistry.get(assignData.electionKey);
-    }).then(function(election) {
-        if(!election) throw new Error("election: " + assignData.electionKey, " Not Found!!!");
-        var factory = getFactory();
-        var relationship = factory.newRelationship('org.elss.electionledger','ElectionLedger', assigndata.ledgerKey);
-        election.electionledger = relationship;
+// function AssignAll(assigndata){
+//     var electionRegistry={}
+//     return getAssetRegistry('org.elss.election.Election').then(function(registry){
+//         electionRegistry = registry
+//         return electionRegistry.get(assignData.electionKey);
+//     }).then(function(election) {
+//         if(!election) throw new Error("election: " + assignData.electionKey, " Not Found!!!");
+//         var factory = getFactory();
+//         var relationship = factory.newRelationship('org.elss.electionledger','ElectionLedger', assigndata.ledgerKey);
+//         election.electionledger = relationship;
         
-        var relationship_box = factory.newRelationship('org.elss.votingbox', 'VotingBox', assigndata.boxKey);
-        election.votingbox = relationship_box;
+//         var relationship_box = factory.newRelationship('org.elss.votingbox', 'VotingBox', assigndata.boxKey);
+//         election.votingbox = relationship_box;
 
-        var relationship_atd = factory.newRelationship('org.elss.attendance', 'Attendance', assigndata.attendanceKey);
-        election.attendance = relationship_atd;
+//         var relationship_atd = factory.newRelationship('org.elss.attendance', 'Attendance', assigndata.attendanceKey);
+//         election.attendance = relationship_atd;
        
-        return electionRegistry.update(election);
-    }).then(function(){
-        var event = getFactory().newEvent('org.elss.election', 'AllAssigned');
-        event.electionKey = assigndata.electionKey;
-        event.ledgerKey = assigndata.ledgerKey;
-        event.boxKey = assigndata.boxKey;
-        event.attendancekey = assigndata.attendanceKey;
-        emit(event);
-    }).catch(function(error){
-        throw new Error(error);
-    });
-}
+//         return electionRegistry.update(election);
+//     }).then(function(){
+//         var event = getFactory().newEvent('org.elss.election', 'AllAssigned');
+//         event.electionKey = assigndata.electionKey;
+//         event.ledgerKey = assigndata.ledgerKey;
+//         event.boxKey = assigndata.boxKey;
+//         event.attendancekey = assigndata.attendanceKey;
+//         emit(event);
+//     }).catch(function(error){
+//         throw new Error(error);
+//     });
+// }
 
-/**
-* 
-* Create Election Ledger Transaction
-* @param {org.elss.election.createElectionLedger} ElectionLedgerData
-* @transaction
-*/
+// /**
+// * 
+// * Create Election Ledger Transaction
+// * @param {org.elss.election.createElectionLedger} ElectionLedgerData
+// * @transaction
+// */
 
-// asset ElectionLedger identified by key {
-//     o String key
-//     o School school
-//     o DateTime expiry
-//     o VoterInfo[] info optional
-//   }
+// // asset ElectionLedger identified by key {
+// //     o String key
+// //     o School school
+// //     o DateTime expiry
+// //     o VoterInfo[] info optional
+// //   }
  
-//   concept VoterInfo {
-//     o String id
-//     o String name
-//     o School school
-//     o Contact contact
-//   }
+// //   concept VoterInfo {
+// //     o String id
+// //     o String name
+// //     o School school
+// //     o Contact contact
+// //   }
  
-//   // Transaction and Event
+// //   // Transaction and Event
  
-//   transaction createElectionLedger{
-//     o School school
-//     o DateTime expiry
-//   }
+// //   transaction createElectionLedger{
+// //     o School school
+// //     o DateTime expiry
+// //   }
  
-//   event ElectionLedgerCreated{
-//     o String key
-//   }
+// //   event ElectionLedgerCreated{
+// //     o String key
+// //   }
 
- function createElecLedger(ElectionLedgerData){
-   return getAssetRegistry('org.elss.electionledger.ElectionLedger')
-    .then(function(electionLedgerRegistry){
+//  function createElecLedger(ElectionLedgerData){
+//    return getAssetRegistry('org.elss.electionledger.ElectionLedger')
+//     .then(function(electionLedgerRegistry){
 
-       //Now add the Election
-       var factory = getFactory();
-       var namespace = 'org.elss.electionledger';
+//        //Now add the Election
+//        var factory = getFactory();
+//        var namespace = 'org.elss.electionledger';
 
-       var ledgerKey = generateElectionKey(ElectionLedgerData.school);
-       var ledger = factory.newResource(namespace, 'ElectionLedger', ledgerKey);
+//        var ledgerKey = generateElectionKey(ElectionLedgerData.school);
+//        var ledger = factory.newResource(namespace, 'ElectionLedger', ledgerKey);
 
-       ledger.school = ElectionLedgerData.school;
-       ledger.expiry = ElectionLedgerData.expiry;
+//        ledger.school = ElectionLedgerData.school;
+//        ledger.expiry = ElectionLedgerData.expiry;
 
-       var event = factory.newEvent(namespace, 'ElectionLedgerCreated');
-       event.key = ledgerKey;
-       emit(evnet);
+//        var event = factory.newEvent(namespace, 'ElectionLedgerCreated');
+//        event.key = ledgerKey;
+//        emit(evnet);
 
-       return electionLedgerRegistry.add(ledger);
+//        return electionLedgerRegistry.add(ledger);
 
- })
-}
+//  })
+// }
 
- function generateLedgerKey(school){
-   var dt = new Date.now();
+//  function generateLedgerKey(school){
+//    var dt = new Date.now();
 
-   var month = dt.getMonth();
-   var dayNum = dt.getDate();
+//    var month = dt.getMonth();
+//    var dayNum = dt.getDate();
 
-   return school+'-'+month+'-'+dayNum;
-}
+//    return school+'-'+month+'-'+dayNum;
+// }
